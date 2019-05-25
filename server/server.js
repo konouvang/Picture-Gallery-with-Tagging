4cc64a2f1c48d3c34138ea62525ec4c8fbf7c315
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-05-25 17:35:04 -0500

    image get works in postman

--- server/server.js
+++ server/server.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
+const images = require('./routes/images.router');
+const tags = require('./routes/tags.router');
+const imagestags = require('./routes/images-tags.router');
 
 /** ---------- MIDDLEWARE ---------- **/
+app.use(bodyParser.urlencoded({extended: true})); // added - Scott's video Many on Many Server - KV
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
-// app.use('/images', images);
-// app.use('/tags', tags);
-// app.use('/images/tags', imagestags);
+app.use('/images', images);
+app.use('/tags', tags);
+app.use('/images/tags', imagestags);
 
 /** ---------- START SERVER ---------- **/
-app.listen(port, function () {
-    console.log('Listening on port: ', port);
+app.listen(PORT, function () {
+    console.log('Listening on port: ', PORT);
 });
